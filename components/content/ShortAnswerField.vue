1c1b656e509403a83c8751dff6202266638863d4
Marlene Terra <crocpeigne@protonmail.com>
2025-07-10 07:45:19 +0200

    Pour pouvoir reset le composant à l'action du bouton Nouvel essai

---
+++ components/content/ShortAnswerField.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import {computed, ref} from 'vue'
+
+    //const submited = inject('submited')
+
+    const userAnswer = ref('') //initialise une variable réactive pour stocker la réponse de l'utilisateur 
+
+</script>
+
+<template>
+    
+    <UTextarea v-model="userAnswer" :maxrows="4" autoresize class="w-full"/>
+
+</template>
