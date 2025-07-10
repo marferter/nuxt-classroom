c03c5325904dba1105863988cf43fd0ab611ce98
Marlene Terra <crocpeigne@protonmail.com>
2025-07-10 07:50:11 +0200

    Changé le nom du composant solution

---
+++ components/content/ActivityShortAnswer.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+    import {computed, ref} from 'vue'
+    import hljs from 'highlight.js'; // Import highlight.js
+
+    const props = defineProps({
+        title : {
+            type: String,
+            required : true
+        },
+        maxScore : {
+            type : Number,
+            required : false
+        }
+    });
+
+    const userAnswer = ref('') //initialise une variable réactive pour stocker la réponse de l'utilisateur 
+
+    //const submited = inject('submited')
+    //const explain = inject('explain')
+    
+</script>
+
+
+<template>
+
+    <Awrapper :title="title">
+
+        <slot></slot>
+
+        <ShortAnswerField/>
+
+        <MdSolution>
+            <slot name="solution"></slot>
+        </MdSolution>
+        
+    </Awrapper>
+
+</template>
