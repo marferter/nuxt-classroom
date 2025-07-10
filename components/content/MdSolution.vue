59a775120da2748f9c0e8216f09a41990c3ef04a
Marlene Terra <crocpeigne@protonmail.com>
2025-07-10 14:07:27 +0200

    spprimé ancien code inutile

--- components/content/MdSolution.vue
+++ components/content/MdSolution.vue
@@ -7,11 +7,8 @@
 
 <template>
 
-    <div v-if="explain" class="text-sm ml-5 mt-1 mb-5 flex space-x-2">
+    <div v-if="explain" class="text-sm ml-5 mt-1 mb-5">
          <slot/>
     </div>
 
-    <!--        <div v-if="explain" class="text-sm ml-5 mt-1 mb-5 space-x-2">
-            {{ options[0].explanations }}
-        </div>-->
 </template>
