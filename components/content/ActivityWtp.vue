d1955aae6a2e8a42b9a80aef1f661f7c13a8c6d7
Marlene Terra <crocpeigne@protonmail.com>
2025-07-10 07:34:30 +0200

    Renommage plus général de ce composant aussi utilisé dans ActivityWtp

---
+++ components/content/ActivityWtp.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+    import {computed, ref} from 'vue'
+    import hljs from 'highlight.js'; // Import highlight.js
+
+    const props = defineProps({
+        title : {
+            type: String,
+            required : true
+        },
+        initialCode : {
+            type: String,
+            required : false
+        },
+        maxScore : {
+            type : Number,
+            required : false
+        }
+    });
+
+    const userAnswer = ref('') //initialise une variable réactive pour stocker la réponse de l'utilisateur 
+
+    //const submited = inject('submited')
+    //const explain = inject('explain')
+    
+</script>
+
+<template>
+
+    <Awrapper :title="title">
+
+        <slot></slot>
+
+        <Wtp :initial-code="initialCode"/>
+
+        <MdSolution>
+            <slot name="solution"></slot>
+        </MdSolution>
+        
+    </Awrapper>
+
+</template>
