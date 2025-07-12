26324f0009de6f67075086f96abe7965972b1e82
Marlene Terra <crocpeigne@protonmail.com>
2025-07-12 18:21:30 +0200

    ajout de l'authentification sue les pages content

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 
+const { login } = useDirectusAuth();
+
 const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
+
+await login({ email: "admin@21-learning.com", password: "Van.Rossum$21" });
+
 </script>
 
 <template>
