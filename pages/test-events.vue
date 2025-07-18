5fb7ce44b4b82278a8f95d3482ff10d89b1cca50
Marlene Terra <crocpeigne@protonmail.com>
2025-07-18 11:23:38 +0200

    page pour tester les nouvelles versions de activity

---
+++ pages/test-events.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ActivityTest3 from '~/components/content/ActivityTest3.vue';
+
+</script>
+
+<template>
+    <ActivityTest3 title="Test de passage d'events et de props ; gestion dans Activity"></ActivityTest3>
+
+    <ActivityTest4 title="Test de passage d'events et de props ; gestion dans Awrapper">
+
+        <template #solution>
+            <p>Ceci est la solution.</p>
+        </template>
+    </ActivityTest4>
+</template>
