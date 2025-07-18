06154b89763c88f45bb7afc156cef00c2fd826fb
Marlene Terra <crocpeigne@protonmail.com>
2025-07-18 11:20:02 +0200

    essai d'amélioration de la communication entres les couches du composant activity (refs, props, events) ; essai de gérer un max de choses dans la couche awrapper

---
+++ components/content/Awrapper4.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, provide, defineEmits} from 'vue'
+
+import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css'
+
+const props = defineProps({
+    title : {
+      type: String,
+      required : false
+    },
+
+    submitted : {
+        type : Boolean,
+        default : false
+    },
+
+    explained : {
+        type : Boolean,
+        default : false
+    }
+
+})
+
+//const emit = defineEmits(['submit','explain'])
+
+const toggleSubmited = () => {
+  submited.value = !submited.value;
+  if (!submited.value){
+    resetKey.value++
+  }
+  if (!submited.value && explain.value) {
+    toggleExplain();
+  }
+}
+
+const submit = () => {
+    //toggleSubmited()
+    emit('submit')
+} 
+
+</script>
+
+
+<template>
+  <UCard class="mt-4" variant="outline">
+    <template #header>
+        <div class="flex space-x-2">
+            <h2 class="text-primary">
+                {{ title }}
+            </h2>
+
+        </div>
+    </template>
+
+    <main>
+      <slot :key="resetKey" ></slot>
+    </main>
+
+    <template #footer>
+        <p> Reçoit la valeur de submitted en prop : {{ submitted }} </p>
+
+        <p> Reçoit la valeur de explained en prop : {{ explained }} </p>
+        <div class="flex space-x-4 justify-end">
+
+            <!--<UButton v-if="submited" @click="toggleExplain" size="xl"> {{ explain ? "Masquer explications" : "Afficher explications" }} </UButton>-->
+
+            <UButton v-if="submitted" @click="$emit('explain')" size="xl"> {{ explained ? "Masquer explications" : "Afficher explications" }} </UButton>
+            
+            <UButton @click="$emit('submit')" size="xl"> {{ submitted ? "Nouvel essai" : "Soumettre" }} </UButton>
+
+        </div>
+    </template>
+  </UCard>
+</template>
---
+++ components/content/MdSolution4.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <div class="text-sm ml-5 mt-1 mb-5">
+         <slot/>
+    </div>
+
+</template>
