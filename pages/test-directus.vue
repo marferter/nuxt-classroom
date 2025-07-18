73b16d233062abc562bf03c3a217b6da179a42ef
Marlene Terra <crocpeigne@protonmail.com>
2025-07-18 11:24:24 +0200

    Page pour tester l'écriture dans directus.

--- pages/test-directus.vue
+++ pages/test-directus.vue
@@ -1,3 +1,34 @@
+<script setup>
+const { $directus, $readItems, $createItem } = useNuxtApp()
+import { ref } from 'vue'
+
+const { data, error } = await useAsyncData('global', async () => {
+  return await $directus.request($readItems('global'))
+})
+
+const global = data
+
+const userAnswer = ref('')
+const sending = ref(false)
+const sendError = ref(null)
+const sendSuccess = ref(false)
+
+async function submitAnswer() {
+  sending.value = true
+  sendError.value = null
+  sendSuccess.value = false
+  try {
+    // Remplace 'answers' par le nom de ta collection Directus
+    await $directus.request($createItem('test_form_simple', { text: userAnswer.value }))
+    sendSuccess.value = true
+    userAnswer.value = ''
+  } catch (e) {
+    sendError.value = e.message || 'Erreur lors de l\'envoi'
+  } finally {
+    sending.value = false
+  }
+}
+</script>
 
 <template>
   <div v-if="global">
@@ -7,6 +38,13 @@
   <div v-else>
     <p>Chargement des données...</p>
   </div>
+
+  <form @submit.prevent="submitAnswer" class="space-y-4 mt-6">
+    <UTextarea v-model="userAnswer" :maxrows="4" autoresize class="w-full" placeholder="Votre réponse..."/>
+    <UButton type="submit" :loading="sending">Envoyer</UButton>
+    <div v-if="sendSuccess" class="text-green-600">Réponse envoyée !</div>
+    <div v-if="sendError" class="text-red-600">{{ sendError }}</div>
+  </form>
 </template>
 
 <!-- c'est le scrip qu'à écrit Cédric avec les composables nuxt-directus ; il marche
@@ -43,15 +81,7 @@ onMounted(async () => {
 
 <!--avant ça ne marchait pas, et j'ai remplacé readItem par readItems-->
 
-<script setup>
-const { $directus, $readItems } = useNuxtApp()
 
-const { data, error } = await useAsyncData('global', async () => {
-  return await $directus.request($readItems('global'))
-})
-
-const global = data
-</script>
 
 
 
