2501694d11757b64689b37a639263d702868105b
Marlene Terra <crocpeigne@protonmail.com>
2025-07-28 12:45:30 +0200

    suppression du log qui faisait 3 km de warns

--- app.vue
+++ app.vue
@@ -2,7 +2,8 @@
  Test pour voir si le plugin directus fonctionne -->
 <script setup>
 const { $directus } = useNuxtApp();
-console.log($directus);
+//console.log($directus); // c'était la source de plein de warns Failed to stringify parce que $directus n'est pas une simple donnée JSON, c'est l'instance complète du client SDK, un objet complexe rempli de fonctions (request, auth, with, etc.). or on ne peut pas stringifier une chaîne de caractères.
+console.log('Le client Directus existe :', !!$directus);
 </script>
 
 
