9d0299a9a4ef29ff79c6bcbf0e50c106497bd864
Marlene Terra <crocpeigne@protonmail.com>
2025-07-28 12:49:23 +0200

    suppression des configs liées au directus sdk, puisque j'utilise le module suppression du hook, parce que je pensais qu'il pouvait être à l'origine de polution de la console, mais en fait pas ; il est stocké dans une page Hooks et Parsing de notion suppresssion de runtimeconfig à confirmer...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,67 +1,10 @@
 import { defineNuxtConfig } from 'nuxt/config'
-import { createDirectus, rest, createItems } from '@directus/sdk';
 
-const client = createDirectus('https://directus.apps.21-learning.com/').with(rest());
-
-//Token d'API admin (stocke-le dans une variable d'env sécurisée !)
-//const TOKEN = process.env.DIRECTUS_TOKEN;
-
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   components: true,
   hooks: {
-    /*'content:file:beforeParse'(ctx) {
-        // console.log(ctx)
-    },
-    'content:file:afterParse': async (ctx) => {
-        //console.dir("résultat du parsing")
-        //console.dir(ctx.content.body.value, {depth: null})
-
-        const { content } = ctx
-        //console.dir(ctx.content.body.value[0], {depth: 0})
-      
-        const uuids = []
-
-        for (const block of content.body.value) {
-          const tag = block[0]
-          const props = block[1]
-          if (
-            typeof tag === 'string' && 
-            tag.startsWith('activity') &&
-            typeof props.uuid === 'string'){
-            //console.dir(tag)
-            //console.dir(props.uuid)
-            uuids.push(props.uuid)
-          }
-        
-          console.dir(uuids)
-
-        if (uuids.length > 0) {
-        const items = uuids.map((uuid) => ({
-          id: uuid,
-        }));
-
-        console.dir(items)
-
-        try {
-          // Authentification
-          //await client.auth.static(TOKEN);
-
-          await client.request(createItems('activities_test_a', items));
-        } catch (e) {
-          console.error('Erreur lors de la création dans Directus:', e);
-        }
-      }
-
-        }
-
-        //console.dir(uuids)
-
-        
-    },*/
   },
 
   content: {
@@ -92,13 +35,17 @@ export default defineNuxtConfig({
     }
   },
   modules: ['@nuxt/ui-pro', '@nuxt/content', 'nuxt-directus'],
-  runtimeConfig: {
+  directus : {
+    url: 'https://directus.apps.21-learning.com/',
+    autoFetch: false
+  }
+  /*runtimeConfig: {
     public: {
       directus: {
         url: 'https://directus.apps.21-learning.com/'
       }
     }
-  },
+  }*/,
   css: ['~/assets/css/main.css'],
   ui: {
     theme: {
