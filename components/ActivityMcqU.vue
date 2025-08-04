0bfc2313c285dc8f16eab0cc12013d504f1b2727
Marlene Terra <crocpeigne@protonmail.com>
2025-08-04 22:28:35 +0200

    Nouveau composant pour les qcm à réponse unique

---
+++ components/ActivityMcqU.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+
+    import type { McqOption } from '~/utils/mcqOption'
+    import {defaultOption} from '~/utils/mcqOption'
+    import {ref, computed, watch} from 'vue'
+
+    const props = defineProps<{
+    options: McqOption[];
+    uuid: string;
+    title: string;
+    maxScore?: number;
+    }>();
+
+    const type = 'mcq'
+
+    // Variables liées à la donnée de la question et aux réponses ; elles sont passées au composant interne d'affichage des questions McqOptionRender
+
+    // La normalisation suivante permet de gérer le cas où aucune prop n'est passée ainsi que d'attribuer des valeurs par défaut aux propriétés ; la version suivante est plus élégante et flexible (avec le spread operator) que celle de la version V1 de ce composant ; elle s'appuie sur l'objet mcqOption par défaut.
+    const normalizedOptions = computed(() => {
+        if (!props.options) {
+            return [];
+        }
+        return props.options.map((opt) => ({
+            ...defaultOption,
+            ...opt
+        }));
+        }
+    );
+    
+    const userAnswer = ref<number | null>(null) // index de la réponse sélectionnée
+    
+    // Comme il s'agit d'un champ de réponse spécifique à la question, on précise ici le nom du champ spécifique correspondant dans la base de données
+    const userAnswerCorrectField = 'user_correction'
+    const userAnswerCorrect = computed(() => {
+        if (userAnswer.value === null) return false;
+        return normalizedOptions.value[userAnswer.value]?.is === true;
+    });
+
+    // Variables et fonctions liées à la soumission des réponses et à l'état du composant
+
+    // Initialisation (=récupération des variables et fonctions) depuis le composable de soumission
+    const { sending, sendError, sendSuccess, submitAnswer } = useAnswerSubmission()
+
+    const handleSubmit = () => {
+        submitAnswer(
+            type,
+            props.title,
+            userAnswer.value, 
+            {[userAnswerCorrectField]:userAnswerCorrect.value}
+        )
+    }
+    //fonction spécifique de réinitialisation du "formulaire"
+    const clearBoxes = () => {
+        userAnswer.value = null
+    }
+
+</script>
+
+<template>
+    <Awrapper 
+        :title="title" 
+        @clearForm="clearBoxes"
+        @submit="handleSubmit">
+        
+        <slot></slot>
+
+        <!-- Pour faire le test sur l'array réactif des réponses de l'user et sur celui de la correction
+        <div v-for="answer in userAnswers"> 
+            <p> {{ answer }}</p>
+        </div>
+
+        <div v-for="correction in userAnswersCorrect"> 
+            <p> {{ correction }}</p>
+        </div>
+        -->
+
+        <div v-for="(opt, index) in normalizedOptions" :key="index">
+            <McqOptionRenderU
+            :opt="opt"
+            :selected="userAnswer === index"
+            @select="userAnswer = index"
+            />
+        </div>
+    </Awrapper>
+
+</template>
---
+++ components/content/McqOptionRenderU.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+    import type { McqOption } from '~/utils/mcqOption.ts'
+    import CorrectAnswerIcon from './icons/CorrectAnswerIcon.vue';
+    import WrongAnswerIcon from './icons/WrongAnswerIcon.vue';
+
+    const props = defineProps<{
+        opt: McqOption,
+        selected: boolean
+    }>()
+    const emit = defineEmits(['select'])
+
+    const userAnswerCorrect = computed(() => 
+    props.selected === props.opt.is
+    )
+
+    const submitted = inject('submitted')
+    const explained = inject('explained')
+    
+</script>
+
+<template>
+    <div  
+        :class="{'bg-elevated/50': !submitted, 'bg-red-50': selected && submitted && !userAnswerCorrect,'bg-green-50': selected && submitted && userAnswerCorrect}"
+        
+        class="p-4 border border-neutral-300 rounded-lg shadow-sm flex items-center justify-between">
+
+        <div >
+            <label class="flex items-center space-x-2" >
+                <UCheckbox
+                    color="primary"
+                    icon="i-lucide-squircle"
+                    :model-value="selected"
+                    @update:model-value="emit('select')"
+                    size="xl"
+                />
+                <!-- <div> {{ opt.option }} </div> -->
+                 <div class="mdc-wrapper">
+                    <MDC :value="opt.option" tag="div"/>
+                </div>
+            </label>
+        </div>
+
+        <CorrectAnswerIcon v-if="selected && submitted && userAnswerCorrect"/>
+        <WrongAnswerIcon v-if="selected && submitted && !userAnswerCorrect"/>
+    </div>
+
+    <!--<div class="ml-4 text-neutral-500 text-xs" v-if="explanation"> {{props.explanation}}</div>
+    -->
+
+    <div v-if="explained && opt.explanation && submitted" class="text-sm ml-5 mt-1 mb-5 flex space-x-2">
+        <label class="flex items-center space-x-1">
+            <UCheckbox
+                icon="i-lucide-squircle" 
+                :default-value="opt.is" 
+                disabled/>
+            <div class="mdc-wrapper">
+                <MDC :value="opt.explanation" tag="div"/>
+            </div>
+        </label>
+    </div>
+</template>
+
+<style>
+
+.mdc-wrapper p {
+ margin-top: 0 !important;
+ margin-bottom: 0 !important;
+ line-height: 1 !important;
+}
+
+</style>
