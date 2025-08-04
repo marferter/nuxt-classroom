900c1a7685774c587da85004da02cd83dd6d8cd9
Marlene Terra <crocpeigne@protonmail.com>
2025-08-04 22:29:01 +0200

    page de test provisoire pour le nouveau composant

---
+++ pages/mcq-nouveau.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+<ActivityMcqU
+  :options="[
+    { option: 'Paris', is: true, explanation: 'Paris est la capitale de la France.' },
+    { option: 'Lyon', is: false, explanation: 'Lyon n’est pas la capitale.' },
+    { option: 'Marseille', is: false, explanation: 'Marseille n’est pas la capitale.' }
+  ]"
+  uuid="exemple-uuid-123"
+  title="Quelle est la capitale de la France ?"
+/>
+</template>
