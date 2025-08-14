d0e0e88f2837f43b3820bc9cd33e98eea4ce717b
Marlene Terra <crocpeigne@protonmail.com>
2025-08-14 07:47:13 +0200

    page pour mettre en évidence la structure de l'AST en chantier

--- pages/ast.vue
+++ pages/ast.vue
@@ -22,3 +22,8 @@ const { data } = await useAsyncData('list', () =>
   </ul>
 </template>
 -->
+
+content/test/mcq-simple.md	Un fichier au contenu simple pour explorer la manipulation de l'AST	{"type":"minimal","value":
+
+
+[["h1",{"id":"un-fichier-au-contenu-simple-pour-explorer-la-manipulation-de-last"},"Un fichier au contenu simple pour explorer la manipulation de l'AST"],["activity-mcq-v1",{":options":"[{\"option\":\"Option 1\",\"is\":true,\"explanation\":\"C'est la bonne réponse.\"},{\"option\":\"Option 2\",\"is\":false,\"explanation\":\"C'est incorrect.\"},{\"option\":\"Option 3\",\"is\":false,\"explanation\":\"C'est aussi incorrect.\"}]","title":"Question 1","uuid":"9e3cb5b9-d553-4019-bf85-1634d111d1ff"},["p",{},"Cochez la bonne réponse parmi les options suivantes :"]],["activity-mcq-v1",{":options":"[{\"option\":\"Option A\",\"is\":true,\"explanation\":\"C'est la bonne réponse.\"},{\"option\":\"Option B\",\"is\":false,\"explanation\":\"C'est incorrect.\"},{\"option\":\"Option C\",\"is\":false,\"explanation\":\"C'est aussi incorrect.\"}]","title":"Question 2","uuid":"27ecb20a-6fb6-460f-a07d-02d337901adf"},["p",{},"Cochez la bonne réponse parmi les options suivantes"]]],"toc":{"title":"","searchDepth":2,"depth":3,"links":[]}}
