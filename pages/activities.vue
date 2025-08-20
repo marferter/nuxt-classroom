71666119dc8cec4bf3ef70bddb3ad87a0f4456ea
Marlene Terra <crocpeigne@protonmail.com>
2025-08-21 00:59:39 +0200

    nettoyage de la page activities

--- pages/activities.vue
+++ pages/activities.vue
@@ -1,58 +1,48 @@
 <script setup>
-import { ContentRenderer } from '#components'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
-const pageHackee = ref(
-  {
-    "champ-lambda":'',
-    // "id": "content/activities/something.md",
-    // "title": "Celui de l'activity, à récupérer",
-    "body": {
-      "type": "minimal",
-      "value": [
-        [
-          "activity-mcq",
-          {
-            ":options": "[{\"option\":\"`3 + 4`\\n\",\"is\":false,\"explanation\":\"`3 + 4` est une *expression arithmétique* qui évalue à 7.\\n\"},{\"option\":\"`\\\"Hello, world!\\\"`\\n\",\"is\":true,\"explanation\":\"`\\\\\\\"Hello, world!\\\\\\\"` est une valeur littérale de type `str`.\\n\"},{\"option\":\"`sqrt(100)`\\n\",\"explanation\":\"`sqrt(100)` est une *expression (appel de fonction)* qui évalue à 10.\\n\"},{\"option\":\"`True`\\n\",\"is\":true,\"explanation\":\"`True` est une valeur littérale de type `bool`.\\n\"},{\"option\":\"`2 * (3 + 5)`\\n\",\"explanation\":\"`2 * (3 + 5)` est une *expression arithmétique* qui évalue à 16.\\n\"},{\"option\":\"`19 >= 18`\\n\",\"explanation\":\"`19 >= 18` est une *expression booléenne* qui évalue à `True`.\\n\"}]",
-            "title": "Prompt simple et options avec code inline - syntaxe avec le *littéral block scalar*\n",
-            "uuid": "f7ca7f81-e1c1-4b0c-916b-6a1809237a6f"
-          }
-        ],
-        [
-          "p",
-          {},
-          "Cochez toutes les valeurs littérales parmi les options suivantes :"
-        ]
-      ]
-    }
-  }
-)
-
-
-const { data: test } = await useAsyncData('activity', () => {
-  return queryCollection('activities')
-    .where('uuid', '=', '2649276c-e5ed-40ff-a509-52fe9349472e')
-    .first()
-})
-
+//requête originale qui renvoie toutes les activités
 const { data: activities } = await useAsyncData('all-activities', () => {
   return queryCollection('activities')
     .all()
 })
 
+
+
 </script>
 
 <template>
-  <main>
-    <!-- <pre> {{ activities [0] }}</pre> -->
-    <pre v-for="(activity, index) in activities"> Activité numéro {{ index }}
-        {{ activity.uuid }}  {{ activity.activityTitle }} </pre>
-    <!-- <pre> Page hackée : {{ pageHackee }}</pre> -->
-    <!-- <ContentRenderer :value="pageHackee" /> -->
-    <!-- <pre> test {{ test }}</pre> -->
-    <!-- <ContentRenderer :value="test" /> -->
-    <ContentRenderer v-for="(activity, index) in activities" :value="activity" />
 
+  <main >
+
+    <h1> Banque d'activités </h1>
+
+    <p> Cette page est une ébauche de banque d'activités ; elle présente un rendu de toutes les activités présentes dans les dossiers "demo" et "cours". </p>
+
+    <h2> Fonctionnalités prévues </h2>
+    <ul class="list-disc pl-6">
+      <li> Pour chaque type d'activity, création d'une variante du composant spécifique pour l'"aperçu", plus compacte.</li>
+      <li> Ajout de menus déroulants pour filter et rechercher par type d'activité, par leçon, par module, par UUID, ...</li>
+      <li> Pour faciliter la duplication d'activités dans une autre page de cours : tickboxes pour sélectionner une ou plusieurs activités et bouton "copier".</li>
+    </ul>
+
+    
+
+    <div class=space-y-6> 
+    <div  v-for="(activity, index) in activities">
+      <USeparator />
+        <div class="flex items-center gap-2">
+          <h3> UUID :</h3>
+          <p> {{ activity.uuid }}</p>
+        </div>
+        <div class="flex items-center gap-2">
+          <h3> Tirée de la page :</h3>
+          <p> {{ activity.lessonId }}</p>
+        </div>
+
+      <ContentRenderer  :value="activity" />
+    </div>
+    </div>
 
   </main>
 </template>
