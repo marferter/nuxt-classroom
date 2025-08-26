291d10dc3a606716886c641ec189d6aae90b96b6
Marlene Terra <crocpeigne@protonmail.com>
2025-08-27 00:15:33 +0200

    sf

--- components/content/MdSolution.vue
+++ components/content/MdSolution.vue
@@ -5,7 +5,9 @@
 <template>
     <!-- Solution affichée avec une icône et un slot pour le contenu -->
     <div class="flex gap-1 items-center rounded-md bg-primary-50 p-1 text-sm">
-        <UIcon name="i-lucide-lightbulb" class="size-20 text-primary-500" />
-        <slot />
+        <UIcon name="i-lucide-lightbulb" class="size-10  text-primary-500" />
+        <div>
+            <slot />
+        </div>
     </div>
 </template>
