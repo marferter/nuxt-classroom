1916762109b979a83b424925f8ce34f0ce3672e6
Marlene Terra <crocpeigne@protonmail.com>
2025-09-01 08:33:04 +0200

    suppression de la connextion hardcoded

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -14,26 +14,16 @@ const sourceLink = computed(() => {
   return '/source' + route.path
 })
 
-const { login } = useDirectusAuth();
-
 const user = useDirectusUser();
+// console.log("role", user.value.role)
 
-onBeforeMount(async () => {
-  // Si l'utilisateur n'est pas connecté, lance le login
-  if (!user.value) {
-    //await login({ email: "prof.test@edufr.ch", password: "cintrisinidirictiS27!" })
-    await login({ email: "admin@21-learning.com", password: "Van.Rossum$21" })
-    //pour vérifier que user.value est bien renseigné
-    //console.log('Connecté !', user.value)
-  } else {
-    //log si déjà connecté
-    console.log('Déjà connecté !', user.value)
-  }
-})
 
 </script>
 
 <template>
+    <header>
+      <UserSwitch :user="user"/>
+    </header>
     <main>
         <ULink 
           :to="sourceLink" 
