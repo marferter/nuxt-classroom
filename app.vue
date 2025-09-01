b3a06f8196207ad9360c453acbba842a306f6e8e
Marlene Terra <crocpeigne@protonmail.com>
2025-09-01 08:30:05 +0200

    suppression du login hardcoded

--- app.vue
+++ app.vue
@@ -1,26 +1,25 @@
-<!-- 
- Test pour voir si le plugin directus fonctionne -->
+<!-- Test pour voir si le plugin directus fonctionne -->
 <script setup>
 const { $directus } = useNuxtApp();
 //console.log($directus); // c'était la source de plein de warns Failed to stringify parce que $directus n'est pas une simple donnée JSON, c'est l'instance complète du client SDK, un objet complexe rempli de fonctions (request, auth, with, etc.). or on ne peut pas stringifier une chaîne de caractères.
 console.log('Le client Directus existe :', !!$directus);
 
-const { login } = useDirectusAuth();
+// const { login } = useDirectusAuth();
 
-const user = useDirectusUser();
+// const user = useDirectusUser();
 
-onBeforeMount(async () => {
-  // Si l'utilisateur n'est pas connecté, lance le login
-  if (!user.value) {
-    //await login({ email: "prof.test@edufr.ch", password: "cintrisinidirictiS27!" })
-    await login({ email: "admin@21-learning.com", password: "Van.Rossum$21" })
-    //pour vérifier que user.value est bien renseigné
-    console.log('Connecté !', user.value)
-  } else {
-    //log si déjà connecté
-    console.log('Déjà connecté !', user.value)
-  }
-})
+// onBeforeMount(async () => {
+//   // Si l'utilisateur n'est pas connecté, lance le login
+//   if (!user.value) {
+//     //await login({ email: "prof.test@edufr.ch", password: "cintrisinidirictiS27!" })
+//     await login({ email: "test-admin@21-learning.com", password: "12345678" })
+//     //pour vérifier que user.value est bien renseigné
+//     console.log('Connecté !', user.value)
+//   } else {
+//     //log si déjà connecté
+//     console.log('Déjà connecté !', user.value)
+//   }
+// })
 </script>
 
 
@@ -30,6 +29,7 @@ onBeforeMount(async () => {
       <NuxtRouteAnnouncer />
 
       <nav>
+
       </nav>
   
       <!-- class="mx-auto max-w-4xl px-4" -->
