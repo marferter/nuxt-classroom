949c215805cc88416b9685967a4b818f65c6b3a0
Marlene Terra <crocpeigne@protonmail.com>
2025-09-10 00:19:09 +0200

    page pour l'accès interdit

---
+++ pages/forbidden.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <h1> Accès interdit </h1>
+    <p>Connectez-vous avec un profil teacher</p>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 3rem;
+    text-align: center;
+    font-weight: bold;
+}
+
+p {
+    font-size: 1rem;
+    text-align: center;
+    font-weight: bold;
+}
+</style>
+
